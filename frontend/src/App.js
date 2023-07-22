8731475ab55ee9980f5e9cdd294e0de2aeb1eddd
Suyog Joshi <suyog@v1040-wn-rt-a-64-51.campus-dynamic.uwaterloo.ca>
2023-07-22 15:44:10 -0400

    frontend initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello This is the leeg app
+      </header>
+    </div>
+  );
+}
+
+export default App;
